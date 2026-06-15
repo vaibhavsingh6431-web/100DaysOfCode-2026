f197b4e9fa059d7aeced0b7a3e77bafcdc352b57
Manas Maheshwari <manas17146@gmail.com>
2026-06-16 00:14:46 +0530

    Remove unnecessary inline comments from Day 2 solution

--- DSA/ManasMaheshwari_590015617/Day_2/RemoveElement.java
+++ DSA/ManasMaheshwari_590015617/Day_2/RemoveElement.java
@@ -4,16 +4,15 @@
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // Pointer for the next valid position
+        int k = 0;
         
         for (int i = 0; i < nums.length; i++) {
-            // If the current element is not the value to remove
             if (nums[i] != val) {
-                nums[k] = nums[i]; // Place it at the valid position
-                k++; // Increment the valid position pointer
+                nums[k] = nums[i];
+                k++;
             }
         }
         
-        return k; // Return the number of elements not equal to val
+        return k;
     }
 }
