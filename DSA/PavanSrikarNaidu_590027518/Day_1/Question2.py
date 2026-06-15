a73633ddc3756fcbd08720840d4f5bbc360c2d89
vaibhav singh <vaibhavsingh6431@gmail.com>
2026-06-15 15:12:10 +0530

    Day 1 Questions

--- DSA/PavanSrikarNaidu_590027518/Day_1/Question2.py
+++
@@ -1,8 +0,0 @@
-arr = [12, 35, 1, 10, 34, 1]
-
-total = 0
-
-for num in arr:
-    total += num
-
-print("Sum of array elements:", total)
---
+++ DSA/vaibhav_590021663/Question2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int sum_of_array(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cout<<"enter number of element:";
+    cin>>n;
+    int arr[n];
+    printf("enter elements: ");
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+  cout<<"sum ="<<sum_of_array(arr,n);
+    return 0;
+}
